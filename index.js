169588afefcb50488af1683d2fadeee5e28a4219
Orta Therox <orta.therox@gmail.com>
2021-12-02 14:33:30 +0000

    Support modern webpack

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ function FindReplacePlugin(options = {}) {
 	this.rules = options.rules;
 };
 
-FindReplacePlugin.prototype.apply = function(compiler) {
+FindReplacePlugin.prototype.apply = function(_compiler) {
+	/** @type {import("webpack").Compiler} */
+	const compiler = _compiler
+
 	const folder = compiler.options.context;
 	const src = path.join(folder, this.src);
 	const dest = path.join(folder, this.dest);
-	compiler.plugin('done', (statsData) => {
-			const stats = statsData.toJson();
-			let template = fs.readFileSync(src, 'utf8');
-			template = this.rules.reduce(
-				(template, rule) => template.replace(
-					rule.find, rule.replace.bind(global, stats)
-				),
-				template
-			);
-			fs.writeFileSync(dest, template);
+
+	compiler.hooks.done.tap('find-replace', (statsData) => {
+		const stats = statsData.toJson();
+		let template = fs.readFileSync(src, 'utf8');
+		template = this.rules.reduce(
+			(template, rule) => template.replace(
+				rule.find, rule.replace.bind(global, stats)
+			),
+			template
+		);
+		fs.writeFileSync(dest, template);
 	});
 };
+
 
 module.exports = FindReplacePlugin;
